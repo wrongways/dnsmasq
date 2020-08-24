8dccb8c2f07f51db16315b51852d376da79233b6
Jez-Wain <jez.wain@ibm.com>
2020-08-24 17:46:37 -0600

    Add prompt and print out blacklist

--- inspect_hosts.py
+++ inspect_hosts.py
@@ -5,8 +5,25 @@ dodgy = ["ad.", "ads", "adv", "track", "google", "facebook", "fb", "insight",
 		 "pixel", "click", "target", "stat", "deal", "tag", "anal",
 		 "classifieds"]
 		 
-with open(hosts) as f:
-	for line in f:
+to_blacklist = []
+
+with open("hosts", "r+") as f:
+	lines = f.readlines()
+	f.seek(0)
+	for line in lines:
+		host = line.strip().split()[1]
 		for trigger in sorted(dodgy):
-			if trigger in line:
-				print(line)
+			if trigger in host:
+				print(host)
+				resp = input("Delete? ").lower()
+				if resp == "y":
+					lines_to_delete.append(linenum)
+					to_blacklist.append(host)
+					break
+				else:
+					f.write(line)
+
+	f.truncate()
+	
+for host in blacklist:
+	print(f"address=/{host}/#")

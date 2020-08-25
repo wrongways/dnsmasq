5535a658810a3230f5a23d92bb4a480b3e6ea0f0
Jez-Wain <jez.wain@ibm.com>
2020-08-25 14:20:00 -0600

    Refactor optimize_blacklist.py to smaller functions

--- optimize_blacklist.py
+++ optimize_blacklist.py
@@ -15,54 +15,68 @@ def is_subdomain(top_domain, domain_to_test):
 	return top_domain_list == test_domain_list[-len(top_domain_list):]
 	
 
-blacklist = utils.load_blacklist()
-
-print(f"Got blacklist: {len(blacklist)} entries")
-
-
-# check for duplicates
-for domain, file_refs in blacklist.items():
-	if len(file_refs) > 1:
-		print(domain)
-		for (file_path, linenum) in file_refs:
-			print("  ", file_path.name, linenum)
-
+def domains_by_depth(blacklist):
 # build a dict with domain depth as key (.com = 1, .co.uk = 2, ibm.com=2 etc)
-domain_depth_map = {}
-for domain in blacklist:
-	components = domain.split('.')
-	depth = len(components)
-	domains_for_depth = domain_depth_map.get(depth, [])
-	domains_for_depth.append(domain)
-	domain_depth_map[depth] = domains_for_depth
-	
+	domain_depth_map = {}
+	for domain in blacklist:
+		components = domain.split('.')
+		depth = len(components)
+		domains_for_depth = domain_depth_map.get(depth, [])
+		domains_for_depth.append(domain)
+		domain_depth_map[depth] = domains_for_depth
+
+	# sort domain_depth_map by ascending depth
+	return {k: domain_depth_map[k] for k in sorted(domain_depth_map.keys())}
 
 
-# sort domain_depth_map by ascending depth
-domain_depth_map = {k: domain_depth_map[k] for k in sorted(domain_depth_map.keys())}
-
-
-for k, v in domain_depth_map.items():
-	print(f"depth {k} has {len(v)} domains")
-	
+def check_for_duplicates(blacklist):
+	for domain, file_refs in blacklist.items():
+		if len(file_refs) > 1:
+			print(domain)
+			for (file_path, linenum) in file_refs:
+				print("  ", file_path.name, linenum)
+				
+				
 # for each depth, find/remove any deeper domains covered by a rule at this depth
+def check_for_overlaps(blacklist, remove=False):
+	domain_depth_map = domains_by_depth(blacklist)
+	depths = sorted(domain_depth_map.keys())
+	for i, depth in enumerate(depths[:-2]):
+		domains_for_depth = domain_depth_map[depth]
+		for top_domain in domains_for_depth:
+			for lower_depth in depths[i+1:]:
+				lower_domains = domain_depth_map[lower_depth]
+				for lower_domain in lower_domains:
+					if is_subdomain(top_domain.strip(), lower_domain.strip()):
+						(lower_file_path, lower_linenum) = blacklist[lower_domain][0]
+						(upper_file_path, upper_linenum) = blacklist[top_domain][0]
+						print(f"{lower_domain}"
+							  f" ({lower_file_path.name}, line {lower_linenum:,})"
+							  f" obseleted by {top_domain}"
+							  f" ({upper_file_path.name}, line {upper_linenum:,})")
+						
+						if remove:
+							lower_domains.remove(lower_domain)
 
-depths = sorted(domain_depth_map.keys())
 
-# for depth in depths:
-# 	print(depth)
-# 	domains = domain_depth_map[depth]
-# 	for domain in domains:
-# 		print("  ", domain)
+	
+
+def print_depthmap_info(domain_depth_map, verbose=False):
+	print(f"Blacklist with {len(blacklist)} entries")
+	for k, v in domain_depth_map.items():
+		print(f"depth {k} has {len(v)} domains")
+
+	if verbose:
+		for depth in domain_depth_map:
+			print(depth)
+			domains = domain_depth_map[depth]
+			for domain in domains:
+				print("  ", domain)
+	
+
+blacklist = utils.load_blacklist()
+check_for_duplicates(blacklist)
+check_for_overlaps(blacklist)
 
 
 
-for i, depth in enumerate(depths[:-2]):
-	domains_for_depth = domain_depth_map[depth]
-	for top_domain in domains_for_depth:
-		for lower_depth in depths[i+1:]:
-			lower_domains = domain_depth_map[lower_depth]
-			for lower_domain in lower_domains:
-				if is_subdomain(top_domain.strip(), lower_domain.strip()):
-					lower_domains.remove(lower_domain)
-					print(f"{lower_domain} obseleted by {top_domain}")

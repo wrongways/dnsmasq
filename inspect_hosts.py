ffc3a6fd2c6560314d1142fc44c18ff2ec190d53
Jez-Wain <jez.wain@ibm.com>
2020-08-24 19:29:41 -0600

    Refactored dodgy detection, and improved whitelist

--- inspect_hosts.py
+++ inspect_hosts.py
@@ -5,26 +5,37 @@ dodgy = ["ad.", "ads", "adv", "track", "google", "facebook", "fb", "insight",
 		 "pixel", "click", "target", "stat", "deal", "tag", "anal",
 		 "classifieds"]
 		 
-whitelist = ["static", "googlevideo", "cloudfront.net"]
+whitelist = ["static", "googlevideo", "cloudfront.net", "adventures", "adjust",
+			 "state", "upload", "roads", "reads", "youtube", "ytimg", "ajax",
+			 "googleapis", "loopinsight", "advance", "leads", "spotify", "typepad",
+			 "advisor", "cloudflare.net", "bluemix", "advocate", "stage"]
 		 
 blacklist = []
+
+def is_dodgy(host):
+	for trigger in whitelist:
+		if trigger in host:
+			return False
+		
+	for trigger in dodgy:
+		if trigger in host:
+			return True
+	
+	return False
+
 
 with open("hosts", "r+") as f:
 	lines = f.readlines()
 	f.seek(0)
 	for line in lines:
 		host = line.strip().split()[1]
-		for trigger in sorted(dodgy):
-			if trigger in host:
-				for ok in whitelist:
-					if ok in host:
-						break
-				resp = input(f"Delete {host}? ").lower()
-				if resp == "y":
-					blacklist.append(host)
-					break
-				else:
-					f.write(line)
+		if is_dodgy(host):
+			resp = input(f"Delete {host}? ").lower()
+			if resp == "y":
+				blacklist.append(host)
+				continue
+		
+		f.write(line)
 
 	f.truncate()
 	
